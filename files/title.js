4d70e2c13d0d18c2be9dd1552536ae92d0a0f815
ezeo <101295286+ezeoxx@users.noreply.github.com>
2022-03-22 11:10:32 -0400

    Create title.js

---
+++ files/title.js
@@ -0,0 +1,3 @@
+var i=0;setInterval(function(){var titles=["e","ez","eze","ezeo ","ezeo :","ezeo :)","ezeo :","ezeo ","eze","ez","e","   ",]
+if(i===titles.length){i=0;}
+document.title=titles[i];i++;},900);
