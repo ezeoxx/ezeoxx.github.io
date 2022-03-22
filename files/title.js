4abd86a3df9f33fb7838733e45b0540f7e809f8b
ezeo <101295286+ezeoxx@users.noreply.github.com>
2022-03-22 11:17:36 -0400

    Delete title.js

--- files/title.js
+++
@@ -1,3 +0,0 @@
-var i=0;setInterval(function(){var titles=["e","ez","eze","ezeo ","ezeo :","ezeo :)","ezeo :","ezeo ","eze","ez","e","   ",]
-if(i===titles.length){i=0;}
-document.title=titles[i];i++;},900);
